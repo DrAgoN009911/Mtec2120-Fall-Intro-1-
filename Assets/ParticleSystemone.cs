91b89b727c8b21798b0b6206a09924fad1f04b3b
DrAgoN009911 <47123052+DrAgoN009911@users.noreply.github.com>
2022-10-07 18:55:28 -0400

    Making new particle systems with new input

---
+++ Assets/ParticleSystemone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class ParticleSystemone : MonoBehaviour
+{
+    
+    public void onSpray(InputAction.CallbackContext context)
+    {
+        Debug.Log("Spray!!");
+    }
+
+    public void onSpraytwo(InputAction.CallbackContext context)
+    {
+        Debug.Log("Spray two!!");
+    }
+
+    public void onSpraythree(InputAction.CallbackContext context)
+    {
+        Debug.Log("Spray three!!");
+    }
+}
